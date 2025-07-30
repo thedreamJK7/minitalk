56ddc2545aa3ace69532bbb6d0902fb794c4a4a0
Javokhir Kubaev <jkubaev7@gmail.com>
2025-07-30 12:38:43 +0200

    understand how signal send works

--- client.c
+++ client.c
@@ -6,18 +6,25 @@
 /*   By: javokhir <javokhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:52:39 by javokhir          #+#    #+#             */
-/*   Updated: 2025/07/29 18:36:40 by javokhir         ###   ########.fr       */
+/*   Updated: 2025/07/30 12:37:35 by javokhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(int argc, char const *argv[])
-{
-	if (argc == 3)
+int	main(int argc, char *argv[]){
+	if (argc == 2)
 	{
-		printf("%s\n", argv[1]);
-		printf("%s\n", argv[2]);
+		int random_int;
+
+		for(int i=0; i<100; i++)
+		{
+			random_int = rand()%2;
+			printf("random_int: %d\n",random_int);
+			if (random_int == 0)
+				kill(atol(argv[1]), SIGINT);
+			else
+				kill(atol(argv[1]), SIGQUIT);
+		}
 	}
-	return (0);
 }
--- server.c
+++ server.c
@@ -3,21 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkubaev <jkubaev@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: javokhir <javokhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:52:48 by javokhir          #+#    #+#             */
-/*   Updated: 2025/07/29 20:30:50 by jkubaev          ###   ########.fr       */
+/*   Updated: 2025/07/30 12:26:59 by javokhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <signal.h>
+
+void	signal_arrived(int signum)
+{
+	if (signum == SIGINT)
+	{
+		printf("Received SIGINT!\n");
+		printf("haiku_category: japanese\n");
+	}
+	if (signum == SIGQUIT){
+		printf("Received SIGQUIT!\n");
+		printf("haiku_category: western\n");
+	}
+}
 
 int	main(void)
 {
-	struct segaction sa;
-	
+	struct sigaction	sa;
+	int					pid;
+
+	pid = getpid();
+	printf("PID = %d\n", pid);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = &signal_arrived;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
